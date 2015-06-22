58f6c3be358bf2b7400f3807dc240303ad35566c
David.Opperman <davidgopperman@gmail.com>
2015-06-22 22:42:03 +0100

    Tonight's updates.

--- tropics-as-base/scripts/oas-install.sh
+++ tropics-as-base/scripts/oas-install.sh
@@ -2,6 +2,8 @@
 
 set -x
 
+#http://sourceforge.net/projects/xming/ needs to be installed on the host machine so that the Oracle installer can connect to the display value assigned to DISPLAY below.
+
 DISPLAY="$(netstat -rn | grep 'UG' | awk '{print $2}'):0.0"; export DISPLAY
 
 /vagrant-software/oas-10g/disk1/runInstaller -ignoreSysPreReqs
